4b27a16206d8e9b6b6bde32f235c244d8c354b8f
1178004778 <111835859+1178004778@users.noreply.github.com>
2022-08-23 13:49:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 import os
 import random
 
-da=os.environ['DATE.DATA']
+da=os.environ['DATE_DATA']
 today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
